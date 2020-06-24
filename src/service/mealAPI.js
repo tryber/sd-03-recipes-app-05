2b0b6c8a66f84567ebe71b6dcc6249e90a619a69
Gabriel <dreadpool38@gmail.com>
2020-06-24 18:25:18 -0300

    Code Climate

--- src/service/mealAPI.js
+++ src/service/mealAPI.js
@@ -1,27 +1,23 @@
-
 // Pega uma refeição a partir de seu nome
-export async function getByName(name){
-  const meal = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`).then(r => r.json())
-return meal
+export async function getByName(name) {
+  const meal = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`).then((r) => r.json());
+  return meal;
 }
 
 // Pega a lista completa e detalhada de todas as categorias
-export async function getAllCategories(){
-    const categorie = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php').then(r => r.json)
-    return categorie
+export async function getAllCategories() {
+  const categorie = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php').then((r) => r.json);
+  return categorie;
 }
 
 // Pega a lista completa do nome de todas as categorias
-export async function getCategorieList(){
-    const categorie = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list').then(r => r.json)
-    return categorie
+export async function getCategorieList() {
+  const categorie = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list').then((r) => r.json);
+  return categorie;
 }
 
 // Pega todas as refeições de uma categoria específica
-export async function filterByCategorie(categorie){
-    const categorieReturn = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categorie}`).then(r => r.json)
-    return categorieReturn
-
+export async function filterByCategorie(categorie) {
+  const categorieReturn = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categorie}`).then((r) => r.json);
+  return categorieReturn;
 }
-
-
