494116c50bccf4a96024ad53e901f3bdb2ce1abb
Gabriel <dreadpool38@gmail.com>
2020-06-24 19:15:15 -0300

    [REFACTOR] Add some treatment on the results

--- src/service/mealAPI.js
+++ src/service/mealAPI.js
@@ -1,13 +1,13 @@
 // Pega uma refeição a partir de seu nome
 export async function getByName(name) {
   const meal = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`).then((r) => r.json());
-  return meal;
+  return meal.meals;
 }
 
 // Pega a lista completa e detalhada de todas as categorias
 export async function getAllCategories() {
   const categorie = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php').then((r) => r.json());
-  return categorie;
+  return categorie.categories;
 }
 
 // Pega a lista completa do nome de todas as categorias
@@ -19,5 +19,5 @@ export async function getCategorieList() {
 // Pega todas as refeições de uma categoria específica
 export async function filterByCategorie(categorie) {
   const categorieReturn = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categorie}`).then((r) => r.json);
-  return categorieReturn;
+  return categorieReturn.meals;
 }
