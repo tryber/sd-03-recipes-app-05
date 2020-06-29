3345984b1a7b10f770974eed821bb157b23b427b
Rodrigo Brezolin <brezolinr@gmail.com>
2020-06-29 16:07:55 -0300

    style page explorer /CC2

--- src/components/ExplorerScreen.jsx
+++ src/components/ExplorerScreen.jsx
@@ -7,8 +7,12 @@ export default function ExplorerScreen() {
   return (
     <div className="body">
       <section className="btn-group">
-        <Link data-testid="explore-food" to="/explorar/comidas"><button className="btn">Explorar Comidas</button></Link>
-        <Link data-testid="explore-drinks" to="/explorar/bebidas"><button className="btn">Explorar Bebidas</button></Link>
+        <Link data-testid="explore-food" to="/explorar/comidas">
+          <button className="btn">Explorar Comidas</button>
+        </Link>
+        <Link data-testid="explore-drinks" to="/explorar/bebidas">
+          <button className="btn">Explorar Bebidas</button>
+        </Link>
       </section>
       <Footer />
     </div>
