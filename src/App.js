29dc59ac8309289532aa4920522ccdfbfc270771
Wenderson belko <57719936+wendersonbelko@users.noreply.github.com>
2020-07-06 20:19:28 -0300

    refatoração dos links

--- src/App.js
+++ src/App.js
@@ -1,138 +1,28 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import TelaPrincipal from './pages/TelaPrincipal';
-import TPProvider from './contexts/TelaPrincipalContext';
-import Login from './pages/Login';
-import DrinkAndMealExplorer from './components/DrinkAndMealExplorer';
 import './App.css';
-import ExplorerScreen from './components/ExplorerScreen';
-import PDProvider from './contexts/ProducDetailsContext';
-import OEProvider from './contexts/OrigemExplorerContext';
-import Productdetails from './pages/ProductDetails';
-import Inprocess from './pages/InProcess';
-import Donerecipes from './pages/DoneRecipes';
-import FavoriteRecipes from './pages/FavoriteRecipes';
-import OrigemExplorer from './pages/OrigemExplorer';
-import ExplorarIngredients from './pages/ExplorarIngredients';
-import PerfilScreen from './components/PerfilScreen';
-import Notfound from './components/NotFound';
-
-function renderReceitasFeitas() {
-  return (
-    <Route path="/receitas-feitas">
-      <Donerecipes />
-    </Route>
-  );
-}
-
-function renderComidas() {
-  return (
-    <Route exact path="/comidas">
-      <TPProvider>
-        <TelaPrincipal />
-      </TPProvider>
-    </Route>
-  );
-}
-function renderBebibdas() {
-  return (
-    <Route exact path="/bebidas">
-      <TPProvider>
-        <TelaPrincipal />
-      </TPProvider>
-    </Route>
-  );
-}
+import { Switch } from 'react-router-dom';
+import * as rota from './Routes';
 
 function App() {
   return (
     <div>
       <Switch>
-        {renderReceitasFeitas()}
-        {renderComidas()}
-        {renderBebibdas()}
-
-        <Route path="/perfil">
-          <PerfilScreen />
-        </Route>
-
-        <Route path="/explorar/bebidas/ingredientes">
-          <ExplorarIngredients />
-        </Route>
-
-        <Route path="/explorar/comidas/ingredientes">
-          <ExplorarIngredients />
-        </Route>
-
-        <Route exact path="/explorar/">
-          <TPProvider>
-            <ExplorerScreen />
-          </TPProvider>
-        </Route>
-        <Route exact path="/">
-          <TPProvider>
-            <Login />
-          </TPProvider>
-        </Route>
-
-        <Route path="/explorar/comidas/area">
-          <OEProvider>
-            <OrigemExplorer />
-          </OEProvider>
-        </Route>
-
-        <Route path="/explorar/bebidas/area">
-          <Notfound />
-        </Route>
-
-        <Route path="/receitas-favoritas">
-          <PDProvider>
-            <FavoriteRecipes />
-          </PDProvider>
-        </Route>
-
-        <Route path="/comidas/:id/in-progress">
-          <PDProvider>
-            <Inprocess />
-          </PDProvider>
-        </Route>
-
-        <Route path="/bebidas/:id/in-progress">
-          <PDProvider>
-            <Inprocess />
-          </PDProvider>
-        </Route>
-
-        <Route path="/comidas/:id">
-          <PDProvider>
-            <Productdetails />
-          </PDProvider>
-        </Route>
-
-        <Route path="/bebidas/:id">
-          <PDProvider>
-            <Productdetails />
-          </PDProvider>
-        </Route>
-
-        <Route exact path="/explorar/:type">
-          <TPProvider>
-            <DrinkAndMealExplorer />
-          </TPProvider>
-        </Route>
-
-        <Route exact path="/explorar/">
-          <TPProvider>
-            <ExplorerScreen />
-          </TPProvider>
-        </Route>
-
-        <Route exact path="/">
-          <TPProvider>
-            <Login />
-          </TPProvider>
-        </Route>
-
+        {rota.renderReceitasFeitas()}
+        {rota.renderComidas()}
+        {rota.renderBebibdas()}
+        {rota.renderPerfil()}
+        {rota.renderIngredientesB()}
+        {rota.renderIngredientesA()}
+        {rota.renderExplorar()}
+        {rota.renderBarra()}
+        {rota.renderAreaA()}
+        {rota.renderAreaB()}
+        {rota.renderFavoritas()}
+        {rota.renderProgressA()}
+        {rota.renderProgressB()}
+        {rota.renderComidasId()}
+        {rota.renderBebidasId()}
+        {rota.renderExplorarTipo()}
       </Switch>
     </div>
   );
