a7fa3f685d215546d2122e338169571d311c7f3e
Gabriel <dreadpool38@gmail.com>
2020-07-07 17:35:29 -0300

    CC2

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -35,34 +35,71 @@ async function changeIngredientFetch(filterToUse, type) {
   return undefined;
 }
 
+async function handleSubmit(location, radio, store, search, history) {
+  const type = location.pathname.includes('comidas') ? 'comidas' : 'bebidas';
+  if (radio === 'Ingrediente') {
+    const result = await changeIngredientFetch(search, type);
+    if (!result) alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+    else if (result.length === 1) history.push(`/${type}/${result[0].idMeal || result[0].idDrink}`);
+    else store.setContent(result.slice(0, 12));
+  } else if (radio === 'Nome') {
+    const result = await getName(type, search);
+    if (!result) alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+    else if (result.length === 1) history.push(`/${type}/${result[0].idMeal || result[0].idDrink}`);
+    else store.setContent(result.slice(0, 12));
+  } else if (radio === 'first') {
+    if (search.length > 1) return alert('Sua busca deve conter somente 1 (um) caracter');
+    const result = await changeByFirstLetter(type, search);
+    if (!result) alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+    else if (result.length === 1) history.push(`/${type}/${result[0].idMeal || result[0].idDrink}`);
+    else store.setContent(result.slice(0, 12));
+  }
+  return null;
+}
+
+function renderInputs(radio, setRadio) {
+  return (
+    <div>
+      <input
+        checked={radio === 'Ingrediente'}
+        onChange={(e) => setRadio(e.target.value)}
+        type="radio"
+        id="Ingrediente"
+        name="gender"
+        value="Ingrediente"
+        data-testid="ingredient-search-radio"
+      />
+      <label htmlFor="Ingrediente">Ingrediente</label>
+      <input
+        onChange={(e) => setRadio(e.target.value)}
+        checked={radio === 'Nome'}
+        type="radio"
+        id="Nome"
+        name="gender"
+        value="Nome"
+        data-testid="name-search-radio"
+      />
+      <label htmlFor="Nome">Nome</label>
+      <input
+        onChange={(e) => setRadio(e.target.value)}
+        checked={radio === 'first'}
+        type="radio"
+        id="first"
+        name="gender"
+        value="first"
+        data-testid="first-letter-search-radio"
+      />
+      <label htmlFor="first">Primeira letra</label>
+    </div>
+  );
+}
+
 function SearchBar() {
   const [search, setSearch] = useState('');
   const [radio, setRadio] = useState('');
   const store = useContext(TelaPrincipalContext);
   const location = useLocation();
   const history = useHistory();
-
-  async function handleSubmit() {
-    const type = location.pathname.includes('comidas') ? 'comidas' : 'bebidas';
-    if (radio === 'Ingrediente') {
-      const result = await changeIngredientFetch(search, type);
-      if (!result) alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
-      else if (result.length === 1) history.push(`/${type}/${result[0].idMeal || result[0].idDrink}`);
-      else store.setContent(result.slice(0, 12));
-    } else if (radio === 'Nome') {
-      const result = await getName(type, search);
-      if (!result) alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
-      else if (result.length === 1) history.push(`/${type}/${result[0].idMeal || result[0].idDrink}`);
-      else store.setContent(result.slice(0, 12));
-    } else if (radio === 'first') {
-      if (search.length > 1) return alert('Sua busca deve conter somente 1 (um) caracter');
-      const result = await changeByFirstLetter(type, search);
-      if (!result) alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
-      else if (result.length === 1) history.push(`/${type}/${result[0].idMeal || result[0].idDrink}`);
-      else store.setContent(result.slice(0, 12));
-    }
-    return null;
-  }
 
   return (
     <div>
@@ -75,41 +112,12 @@ function SearchBar() {
         className="input-search"
         value={search}
       />
-      <div>
-
-        <input
-          checked={radio === 'Ingrediente'}
-          onChange={(e) => setRadio(e.target.value)}
-          type="radio"
-          id="Ingrediente"
-          name="gender"
-          value="Ingrediente"
-          data-testid="ingredient-search-radio"
-        />
-        <label htmlFor="Ingrediente">Ingrediente</label>
-        <input
-          onChange={(e) => setRadio(e.target.value)}
-          checked={radio === 'Nome'}
-          type="radio"
-          id="Nome"
-          name="gender"
-          value="Nome"
-          data-testid="name-search-radio"
-        />
-        <label htmlFor="Nome">Nome</label>
-        <input
-          onChange={(e) => setRadio(e.target.value)}
-          checked={radio === 'first'}
-          type="radio"
-          id="first"
-          name="gender"
-          value="first"
-          data-testid="first-letter-search-radio"
-        />
-        <label htmlFor="first">Primeira letra</label>
-
-      </div>
-      <button onClick={handleSubmit} type="button" data-testid="exec-search-btn">
+      {renderInputs(radio, setRadio)}
+      <button
+        onClick={() => handleSubmit(location, radio, store, search, history)}
+        type="button"
+        data-testid="exec-search-btn"
+      >
         Buscar
       </button>
     </div>
