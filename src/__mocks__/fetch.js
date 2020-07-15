02a73671ba9fdc2ec0c097546e2e1045f77dfd42
Gabriel <dreadpool38@gmail.com>
2020-07-15 19:26:24 -0300

    voa mlk

--- src/__mocks__/fetch.js
+++ src/__mocks__/fetch.js
@@ -26,101 +26,68 @@ const areas = require('./areas');
 const japaneseMeals = require('./japaneseMeals');
 const italianMeals = require('./italianMeals');
 
+const jsonFunc = (url) => {
+  switch (url) {
+    case 'https://www.themealdb.com/api/json/v1/1/list.php?c=list':
+      return Promise.resolve(mealCategories);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list':
+      return Promise.resolve(drinkCategories);
+    case 'https://www.themealdb.com/api/json/v1/1/list.php?i=list':
+      return Promise.resolve(mealIngredients);
+    case 'https://www.themealdb.com/api/json/v1/1/filter.php?i=Chicken':
+      return Promise.resolve(mealsByIngredient);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list':
+      return Promise.resolve(drinkIngredients);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Light rum':
+      return Promise.resolve(drinksByIngredient);
+    case 'https://www.themealdb.com/api/json/v1/1/list.php?a=list':
+      return Promise.resolve(areas);
+    case 'https://www.themealdb.com/api/json/v1/1/random.php':
+      return Promise.resolve(oneMeal);
+    case 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771':
+      return Promise.resolve(oneMeal);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Aquamarine':
+      return Promise.resolve(oneDrink);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/random.php':
+      return Promise.resolve(oneDrink);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=':
+      return Promise.resolve(oneDrink);
+    case 'https://www.themealdb.com/api/json/v1/1/search.php?s=soup':
+      return Promise.resolve(soupMeals);
+    case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef':
+      return Promise.resolve(beefMeals);
+    case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast':
+      return Promise.resolve(breakfastMeals);
+    case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken':
+      return Promise.resolve(chickenMeals);
+    case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert':
+      return Promise.resolve(dessertMeals);
+    case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Goat':
+      return Promise.resolve(goatMeals);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=gin':
+      return Promise.resolve(ginDrinks);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary Drink':
+      return Promise.resolve(ordinaryDrinks);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail':
+      return Promise.resolve(cocktailDrinks);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Milk / Float / Shake':
+      return Promise.resolve(milkDrinks);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Other/Unknown':
+      return Promise.resolve(otherDrinks);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocoa':
+      return Promise.resolve(cocoaDrinks);
+    case 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=':
+      return Promise.resolve(drinks);
+    default:
+      return Promise.resolve(meals);
+  }
+};
+
 const mockFetch = () => {
   const mocked = (url) => Promise.resolve({
     status: 200,
     ok: true,
-    json: () => {
-      switch (url) {
-        case 'https://www.themealdb.com/api/json/v1/1/list.php?c=list':
-          return Promise.resolve(mealCategories);
-
-        case 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list':
-          return Promise.resolve(drinkCategories);
-
-        case 'https://www.themealdb.com/api/json/v1/1/list.php?i=list':
-          return Promise.resolve(mealIngredients);
-
-        case 'https://www.themealdb.com/api/json/v1/1/filter.php?i=Chicken':
-          return Promise.resolve(mealsByIngredient);
-
-        case 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list':
-          return Promise.resolve(drinkIngredients);
-
-        case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Light rum':
-          return Promise.resolve(drinksByIngredient);
-
-        case 'https://www.themealdb.com/api/json/v1/1/list.php?a=list':
-          return Promise.resolve(areas);
-
-        case 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Japanese':
-          return Promise.resolve(japaneseMeals);
-
-        case 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Italian':
-          return Promise.resolve(italianMeals);
-
-        case 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata':
-          return Promise.resolve(oneMeal);
-        case 'https://www.themealdb.com/api/json/v1/1/random.php':
-          return Promise.resolve(oneMeal);
-        case 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771':
-          return Promise.resolve(oneMeal);
-
-        case 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Aquamarine':
-          return Promise.resolve(oneDrink);
-        case 'https://www.thecocktaildb.com/api/json/v1/1/random.php':
-          return Promise.resolve(oneDrink);
-        case 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=':
-          return Promise.resolve(oneDrink);
-
-        case 'https://www.themealdb.com/api/json/v1/1/search.php?s=soup':
-          return Promise.resolve(soupMeals);
-
-        case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef':
-          return Promise.resolve(beefMeals);
-
-        case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast':
-          return Promise.resolve(breakfastMeals);
-
-        case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken':
-          return Promise.resolve(chickenMeals);
-
-        case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert':
-          return Promise.resolve(dessertMeals);
-
-        case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Goat':
-          return Promise.resolve(goatMeals);
-
-        case 'https://www.themealdb.com/api/json/v1/1/search.php?s=xablau':
-          return Promise.resolve(emptyMeals);
-
-        case 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=gin':
-          return Promise.resolve(ginDrinks);
-
-        case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary Drink':
-          return Promise.resolve(ordinaryDrinks);
-
-        case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail':
-          return Promise.resolve(cocktailDrinks);
-
-        case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Milk / Float / Shake':
-          return Promise.resolve(milkDrinks);
-
-        case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Other/Unknown':
-          return Promise.resolve(otherDrinks);
-
-        case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocoa':
-          return Promise.resolve(cocoaDrinks);
-
-        case 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=xablau':
-          return Promise.resolve(emptyDrinks);
-
-        case 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=':
-          return Promise.resolve(drinks);
-        default:
-          return Promise.resolve(meals);
-      }
-    },
+    json: () => jsonFunc(url),
   });
   global.fetch = jest.fn((url) => mocked(url));
 };
