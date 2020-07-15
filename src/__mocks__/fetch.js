b93e74dc1c078a81a3020cdfe4f1e0a3068f2e88
Gabriel <dreadpool38@gmail.com>
2020-07-15 19:43:41 -0300

    agora vai, tenho fé

--- src/__mocks__/fetch.js
+++ src/__mocks__/fetch.js
@@ -2,11 +2,9 @@ const meals = require('./meals');
 const oneMeal = require('./oneMeal');
 const soupMeals = require('./soupMeals');
 const beefMeals = require('./beefMeals');
-const breakfastMeals = require('./breakfastMeals');
 const chickenMeals = require('./chickenMeals');
 const dessertMeals = require('./dessertMeals');
 const goatMeals = require('./goatMeals');
-const emptyMeals = require('./emptyMeals');
 const mealCategories = require('./mealCategories');
 const mealIngredients = require('./mealIngredients');
 const mealsByIngredient = require('./mealsByIngredient');
@@ -15,16 +13,9 @@ const oneDrink = require('./oneDrink');
 const ginDrinks = require('./ginDrinks');
 const ordinaryDrinks = require('./ordinaryDrinks');
 const cocktailDrinks = require('./cocktailDrinks');
-const milkDrinks = require('./milkDrinks');
-const otherDrinks = require('./otherDrinks');
-const cocoaDrinks = require('./cocoaDrinks');
-const emptyDrinks = require('./emptyDrinks');
 const drinkCategories = require('./drinkCategories');
 const drinkIngredients = require('./drinkIngredients');
-const drinksByIngredient = require('./drinksByIngredient');
 const areas = require('./areas');
-const japaneseMeals = require('./japaneseMeals');
-const italianMeals = require('./italianMeals');
 
 const jsonFunc = (url) => {
   switch (url) {
@@ -38,16 +29,12 @@ const jsonFunc = (url) => {
       return Promise.resolve(mealsByIngredient);
     case 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list':
       return Promise.resolve(drinkIngredients);
-    case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Light rum':
-      return Promise.resolve(drinksByIngredient);
     case 'https://www.themealdb.com/api/json/v1/1/list.php?a=list':
       return Promise.resolve(areas);
     case 'https://www.themealdb.com/api/json/v1/1/random.php':
       return Promise.resolve(oneMeal);
     case 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771':
       return Promise.resolve(oneMeal);
-    case 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Aquamarine':
-      return Promise.resolve(oneDrink);
     case 'https://www.thecocktaildb.com/api/json/v1/1/random.php':
       return Promise.resolve(oneDrink);
     case 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=':
@@ -56,8 +43,6 @@ const jsonFunc = (url) => {
       return Promise.resolve(soupMeals);
     case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef':
       return Promise.resolve(beefMeals);
-    case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast':
-      return Promise.resolve(breakfastMeals);
     case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken':
       return Promise.resolve(chickenMeals);
     case 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert':
@@ -70,16 +55,12 @@ const jsonFunc = (url) => {
       return Promise.resolve(ordinaryDrinks);
     case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail':
       return Promise.resolve(cocktailDrinks);
-    case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Milk / Float / Shake':
-      return Promise.resolve(milkDrinks);
-    case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Other/Unknown':
-      return Promise.resolve(otherDrinks);
-    case 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocoa':
-      return Promise.resolve(cocoaDrinks);
     case 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=':
       return Promise.resolve(drinks);
-    default:
+    case 'https://www.themealdb.com/api/json/v1/1/search.php?s=':
       return Promise.resolve(meals);
+    default:
+      return Promise.resolve(oneDrink);
   }
 };
 
